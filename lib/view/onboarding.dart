0b1f602c119ed3e2aef53896d485befac4ecebb0
munasir <munasirpv@gmail.com>
2021-09-14 17:18:21 +0530

    onboarding.dart

--- lib/view/onboarding.dart
+++ lib/view/onboarding.dart
@@ -4,6 +4,67 @@ class Onboarding extends StatelessWidget {
   const Onboarding({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+              'assets/images/onboarding-bg.png'
+            ),
+            fit: BoxFit.cover
+          )
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Image.asset(
+                'assets/icons/icon.png',
+                height: 60,
+              ),
+              const Text(
+                'Welcome\nto our store',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 48,
+                  fontWeight: FontWeight.w600
+                ),
+              ),
+              Text(
+                'Ger your groceries in as fast as one hour',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: const Color(0xFFFCFCFC).withOpacity(0.7)
+                ),
+              ),
+              const SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: (){},
+                style: ElevatedButton.styleFrom(
+                  primary: Theme.of(context).primaryColor,
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 15
+                  ),
+                ),
+                child: const Text(
+                  'Get Started',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white
+                  ),
+                ),
+              ),
+              const SizedBox(height: 50),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
